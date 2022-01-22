1538a29a83c35827bd2806029afa27e07c73d5bd
Neordinator <abyssguide@gmail.com>
2022-01-22 11:09:23 +0300

    Added ResourceManager

---
+++ src/Resources/ResourceManager.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <memory>
+#include <map>
+
+#include "../Renderer/ShaderProgram.h"
+
+namespace Renderer
+{
+	class ShaderProgram;
+}
+
+class ResourceManager
+{
+public:
+	ResourceManager& operator=(const ResourceManager&) = delete;
+	ResourceManager& operator=(ResourceManager&) = delete;
+	ResourceManager& operator=(ResourceManager&&) = delete;
+	ResourceManager(ResourceManager&&) = delete;
+
+	ResourceManager() = default;
+	ResourceManager(const std::string&);
+
+	std::shared_ptr<Renderer::ShaderProgram> loadShaders(const std::string&, const std::string&, const std::string&);
+	std::shared_ptr<Renderer::ShaderProgram> getShader(const std::string&);
+private:
+	typedef std::map<const std::string, std::shared_ptr<Renderer::ShaderProgram>> ShaderProgramsMap;
+	ShaderProgramsMap m_shaderPrograms;
+
+	std::string m_path;
+
+	std::string getFileString(const std::string&) const;
+};
